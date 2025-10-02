1760ad468aeb17c3a0999c1c4d43e388d8d8c024
group12arucg-admin <alexwebb94@hotmail.co.uk>
2025-10-02 14:55:46 +0100

    added 3 dimension movements with grounded and jumping logic

--- physicsPorject/Assets/Player/CharacterController.cs
+++ physicsPorject/Assets/Player/CharacterController.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
@@ -6,11 +7,14 @@ public class CharacterController : MonoBehaviour
     float acceleration = 2000f;
     float maxSpeed = 8000f;
     private Rigidbody rb;
+    private bool isGrounded;
+    public float jumpForce = 10;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        isGrounded = true;
     }
 
     // Update is called once per frame
@@ -24,6 +28,51 @@ public class CharacterController : MonoBehaviour
             {
                 rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
             }
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(-transform.forward * acceleration * Time.deltaTime, ForceMode.Acceleration);
+
+            if (rb.linearVelocity.magnitude > maxSpeed)
+            {
+                rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-transform.right * acceleration * Time.deltaTime, ForceMode.Acceleration);
+
+            if (rb.linearVelocity.magnitude > maxSpeed)
+            {
+                rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(transform.right * acceleration * Time.deltaTime, ForceMode.Acceleration);
+
+            if (rb.linearVelocity.magnitude > maxSpeed)
+            {
+                rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor" && isGrounded == false)
+        {
+            Debug.Log("grounded");
+            isGrounded = true;
         }
     }
 }
