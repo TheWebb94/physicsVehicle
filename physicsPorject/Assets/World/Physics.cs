7d050d4ba41943ca628ad3a84b74fb5617f9898b
group12arucg-admin <alexwebb94@hotmail.co.uk>
2025-10-16 16:51:33 +0100

    physics now uses CSA properly and drag equation implemented

--- physicsPorject/Assets/World/Physics.cs
+++ physicsPorject/Assets/World/Physics.cs
@@ -1,38 +1,55 @@
 using UnityEngine;
 
+
 public class Physics : MonoBehaviour
 {
 
     public float gravity = -9.81f;
     public Rigidbody rb;
+    [SerializeField] private DragForceManager playerDragForce;
 
     public float A;         // A (cross-sectional area) = needs claculation based on player model
-    public float Cd;        // drag coefficient
+    public float dragCoefficient;        // drag coefficient
+    private float airDensity;
 
 
 
-    /* Cd (drag coefficient) = 1.0 (approximate for a human)
-                             = 0.47 (approximate for a sphere)
-                              = 1.05 (approximate for a cube)
-                             = 0.28 - 0.35 (approximate for a car)
+   
 
-    */
+    
+
+    void Awake()
+    {
+        airDensity = 1.225f;        // p (air density) = 1.225 kg/m^3 at sea level 
+        dragCoefficient = 0.3f; /* Cd (drag coefficient)    = 1.0 (approximate for a human)
+                                                            = 0.47 (approximate for a sphere)
+                                                            = 1.05 (approximate for a cube)
+                                                            = 0.28 - 0.35 (approximate for a car) */
+
+    }
 
     void FixedUpdate()
     {
         //Gravity
         rb.AddForce(new Vector3(0, gravity, 0), ForceMode.Acceleration);
 
-        //wind resistance
-        float dragMagnitude = 0.5f * 1.225f * Mathf.Pow(rb.linearVelocity.magnitude, 2) * Cd * A;
-        rb.AddForce(-rb.linearVelocity.normalized * dragMagnitude, ForceMode.Force);
+        //drag equation Fd = 0.5 * p * v^2 * Cd * A
+        float dragMagnitude = 0.5f * airDensity * VelocitySquared() * dragCoefficient * (float)GetCrossSectionalArea();
+        rb.AddForce(-rb.linearVelocity * dragMagnitude);
 
-        // p (air density) = 1.225 kg/m^3 at sea level  
-        // v (velocity) = rb.linearVelocity.magnitude
+        Debug.Log(dragMagnitude);
+    }
 
-        // Cd (drag coefficient) = 1.0 (approximate for a human)
-        // Fd = 0.5 * p * v^2 * Cd * A
+    private float VelocitySquared()
+    {
+        return Mathf.Pow(rb.linearVelocity.magnitude, 2);
+    }
 
-        //raycast object from infront the vehicle in the direction of the movement vector, aiming towards the vehicle. 
+    private float GetCrossSectionalArea()
+    {
+        var playerRef = UnityEngine.Object.FindFirstObjectByType<DragForceManager>();
+        playerDragForce = playerRef;
+
+        return  playerDragForce.crossSectionalArea;
     }
 }
