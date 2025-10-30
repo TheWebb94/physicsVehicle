4965731e62670ce7eeb169fdd0e9bc5cf63db4cc
group12arucg-admin <alexwebb94@hotmail.co.uk>
2025-10-30 14:47:17 +0000

    class

---
+++ physicsPorject/Assets/Car/Car.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+   // private Engine engine;
+   // private Wheel wheel;
+   // private Brakes brakes;
+   //
+   // public abstract void Drive()
+   //
+   //     switch(vehicleLogic)
+   //     {
+   //     case Trackcar:
+   //         //x
+   //         return;
+   //     case Othercar:
+   //         //x
+   //
+   //
+
+
+}
